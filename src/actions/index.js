b4fac67a4650cad491963f9f10294b222b9f2e2f
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-16 11:06:41 +0300

    axios, jsonPlaceHolder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
+import jsonPlaceHolder from "../api/jsonPlaceHolder";
+
 export const fetchPosts = () => {
+  const response = jsonPlaceHolder.get("/posts");
+
   return {
     type: "FETCH_POSTS",
+    payload: response,
   };
 };
