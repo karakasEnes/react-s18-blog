925861c6f02f9fe8b25ec40e0ac5af9ae6513281
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-16 11:22:21 +0300

    axios

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
 import allReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const store = createStore(allReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={createStore(allReducer)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector("#root")
