3cb5805b401acf511c56d15d74040b357e9f698f
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-16 15:13:32 +0300

    postList, with thunk, mapStateToProps and all details

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,5 +1,10 @@
-const postsReducer = () => {
-  return "postReducer";
+const postsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return action;
+  }
 };
 
 export default postsReducer;
