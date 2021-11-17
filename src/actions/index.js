d2c8cedc62537ad425fe70ebf554a56bdd7e38d5
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-17 14:26:44 +0300

    fetchPostsAndUsers, postList, userHeader refactor for new approach of fetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
 import _ from "lodash";
 import jsonPlaceHolder from "../api/jsonPlaceHolder";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log("posts start");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log("posts fetched...");
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
